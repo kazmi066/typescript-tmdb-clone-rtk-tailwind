13d663708f2967d6049a853681ef1c09d1476152
kazmi066 <awaisabbas47@gmail.com>
2023-05-21 11:17:51 +0500

    Update env variable for api key

--- src/data/endpoints.ts
+++ src/data/endpoints.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = "https://api.themoviedb.org/3";
-const API_KEY = process.env.TMDB_API_KEY;
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const TRENDING_BASE_URL = `${API_BASE_URL}/trending/all/day?api_key=${API_KEY}`;
 const SEARCH_BASE_URL = `${API_BASE_URL}/search/multi?api_key=${API_KEY}&language=en-US`;
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p"
